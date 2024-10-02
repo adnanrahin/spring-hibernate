ffdde69f97247da8824194a2dc975f40dc2159af
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-10-02 19:54:07 -0400

    update

--- spring-restful-service/src/main/java/org/product/restful/api/controller/PaymentController.java
+++ spring-restful-service/src/main/java/org/product/restful/api/controller/PaymentController.java
@@ -1,59 +1,96 @@
 package org.product.restful.api.controller;
 
+import org.info.product.models.Order;
 import org.info.product.models.Payment;
+import org.product.restful.api.dto.PaymentDTO;
 import org.product.info.services.PaymentService;
+import org.product.info.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private final OrderService orderService;
 
     @Autowired
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, OrderService orderService) {
         this.paymentService = paymentService;
+        this.orderService = orderService;
+    }
+
+    private PaymentDTO convertToDTO(Payment payment) {
+        return new PaymentDTO(
+                payment.getPaymentId(),
+                payment.getOrder() != null ? payment.getOrder().getOrderId() : null,
+                payment.getPaymentDate(),
+                payment.getAmount(),
+                payment.getPaymentMethod()
+        );
+    }
+
+    private Payment convertToEntity(PaymentDTO paymentDTO) {
+        Payment payment = new Payment();
+        payment.setPaymentId(paymentDTO.getPaymentId());
+        if (paymentDTO.getOrderId() != null) {
+            Order order = orderService.findOrderById(paymentDTO.getOrderId());
+            payment.setOrder(order);
+        }
+        payment.setPaymentDate(paymentDTO.getPaymentDate());
+        payment.setAmount(paymentDTO.getAmount());
+        payment.setPaymentMethod(paymentDTO.getPaymentMethod());
+        return payment;
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<Payment>> getAllPayments() {
+    public ResponseEntity<List<PaymentDTO>> getAllPayments() {
         List<Payment> payments = paymentService.findAllPayments();
-        return new ResponseEntity<>(payments, HttpStatus.OK);
+        List<PaymentDTO> paymentDTOs = payments.stream().map(this::convertToDTO).collect(Collectors.toList());
+        return new ResponseEntity<>(paymentDTOs, HttpStatus.OK);
     }
 
     @GetMapping("getById/{id}")
-    public ResponseEntity<Payment> getPaymentById(@PathVariable("id") long id) {
+    public ResponseEntity<PaymentDTO> getPaymentById(@PathVariable("id") long id) {
         Payment payment = paymentService.findPaymentById(id);
         if (payment != null) {
-            return new ResponseEntity<>(payment, HttpStatus.OK);
+            PaymentDTO paymentDTO = convertToDTO(payment);
+            return new ResponseEntity<>(paymentDTO, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+    public ResponseEntity<PaymentDTO> createPayment(@RequestBody PaymentDTO paymentDTO) {
+        Payment payment = convertToEntity(paymentDTO);
         Payment savedPayment = paymentService.save(payment);
-        return new ResponseEntity<>(savedPayment, HttpStatus.CREATED);
+        PaymentDTO savedPaymentDTO = convertToDTO(savedPayment);
+        return new ResponseEntity<>(savedPaymentDTO, HttpStatus.CREATED);
     }
 
-    // PUT: Update an existing payment
     @PutMapping("/update/{id}")
-    public ResponseEntity<Payment> updatePayment(@PathVariable("id") long id, @RequestBody Payment paymentDetails) {
+    public ResponseEntity<PaymentDTO> updatePayment(@PathVariable("id") long id, @RequestBody PaymentDTO paymentDTO) {
         Payment existingPayment = paymentService.findPaymentById(id);
         if (existingPayment != null) {
-            existingPayment.setAmount(paymentDetails.getAmount());
-            existingPayment.setOrder(paymentDetails.getOrder());
-            existingPayment.setPaymentDate(paymentDetails.getPaymentDate());
-            existingPayment.setPaymentMethod(paymentDetails.getPaymentMethod());
+            existingPayment.setAmount(paymentDTO.getAmount());
+            existingPayment.setPaymentDate(paymentDTO.getPaymentDate());
+            existingPayment.setPaymentMethod(paymentDTO.getPaymentMethod());
+
+            if (paymentDTO.getOrderId() != null) {
+                Order order = orderService.findOrderById(paymentDTO.getOrderId());
+                existingPayment.setOrder(order);
+            }
 
             Payment updatedPayment = paymentService.save(existingPayment);
-            return new ResponseEntity<>(updatedPayment, HttpStatus.OK);
+            PaymentDTO updatedPaymentDTO = convertToDTO(updatedPayment);
+            return new ResponseEntity<>(updatedPaymentDTO, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -70,4 +107,3 @@ public class PaymentController {
         }
     }
 }
-
---
+++ spring-restful-service/src/main/java/org/product/restful/api/dto/PaymentDTO.java
@@ -0,0 +1,61 @@
+package org.product.restful.api.dto;
+
+import java.util.Date;
+
+public class PaymentDTO {
+    private Long paymentId;
+    private Long orderId;  // Referencing the order by ID
+    private Date paymentDate;
+    private double amount;
+    private String paymentMethod;
+
+    public PaymentDTO(Long paymentId, Long orderId, Date paymentDate, double amount, String paymentMethod) {
+        this.paymentId = paymentId;
+        this.orderId = orderId;
+        this.paymentDate = paymentDate;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public PaymentDTO() {}
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
