ba610532e7bb3742b160fbb27d431c5fa6e1594c
adnanrahin <adnan.rahin13@gmail.com>
2024-10-02 03:10:52 -0400

    updated the schema type to DTO

--- spring-restful-service/src/main/java/org/product/restful/api/controller/CustomerController.java
+++ spring-restful-service/src/main/java/org/product/restful/api/controller/CustomerController.java
@@ -2,6 +2,7 @@ package org.product.restful.api.controller;
 
 import org.product.info.services.CustomerService;
 import org.info.product.models.Customer;
+import org.product.restful.api.dto.CustomerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -17,39 +19,65 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    // Convert Customer to CustomerDTO
+    private CustomerDTO convertToDTO(Customer customer) {
+        return new CustomerDTO(
+                customer.getCustomerId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getPhoneNumber()
+        );
+    }
+
+    // Convert CustomerDTO to Customer
+    private Customer convertToEntity(CustomerDTO customerDTO) {
+        return new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getFirstName(),
+                customerDTO.getLastName(),
+                customerDTO.getEmail(),
+                customerDTO.getPhoneNumber(),
+                null // Assuming orders will be handled separately
+        );
+    }
+
     // Get all customers
     @GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> customers = customerService.findAll();
-        return new ResponseEntity<>(customers, HttpStatus.OK);
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
+        List<CustomerDTO> customersDTO = customerService.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(customersDTO, HttpStatus.OK);
     }
 
     // Get a customer by ID
     @GetMapping("/getById/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
         Customer customer = customerService.findById(id);
         if (customer == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(customer, HttpStatus.OK);
+        return new ResponseEntity<>(convertToDTO(customer), HttpStatus.OK);
     }
 
     // Create a new customer
     @PostMapping("/save")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
-        Customer savedCustomer = customerService.save(customer);
-        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        Customer savedCustomer = customerService.save(convertToEntity(customerDTO));
+        return new ResponseEntity<>(convertToDTO(savedCustomer), HttpStatus.CREATED);
     }
 
     // Update an existing customer
     @PutMapping("/update/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
-        customer.setCustomerId(id); // Assuming you have a setter for customerId
-        Customer updatedCustomer = customerService.save(customer);
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
+        customerDTO.setCustomerId(id);
+        Customer updatedCustomer = customerService.save(convertToEntity(customerDTO));
         if (updatedCustomer == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+        return new ResponseEntity<>(convertToDTO(updatedCustomer), HttpStatus.OK);
     }
 
     // Delete a customer by ID
